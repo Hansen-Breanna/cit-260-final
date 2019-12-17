cfd12ef0d3ed322920d24ad608a490db19613fa5
Mindy Gee <43478774+mgee4@users.noreply.github.com>
2019-12-16 20:18:35 -0600

    Updated House with current info in master

--- src/House.java
+++ src/House.java
@@ -3,6 +3,7 @@
  */
 public class House extends Residence {
 
+    //Lot size in acres of investment property
     private double acreage = 0;
 
     /**
@@ -25,6 +26,7 @@ public class House extends Residence {
      * Default constructor
      */
     House() {
+        //default value of acreage
         acreage = 0;
     };
 
@@ -38,20 +40,45 @@ public class House extends Residence {
      * @param purchasePrice
      * @param taxes
      */
-    House(String address, int bedrooms, double bathrooms, int sqfeet, double acreage, double purchasePrice,
-                        double taxes) {
+    House(String address, int bedrooms, double bathrooms, int sqfeet, double purchasePrice, double taxes,
+          double acreage) {
         super(address, bedrooms, bathrooms, sqfeet, purchasePrice, taxes);
         this.acreage = acreage;
     }
 
     /**
-     * The String method
-     * This method display the data about each property as a string in a table
-     * @return String
+     * The header Method
+     * This method returns the header for the House table
      */
     @Override
-    public String toString() {
-        return String.format(" ");
+    public String tableHeader() {
+        //Return string representation of the House table header
+        return String.format("%nHouse Properties List%n%-50s   %-4s   %-5s   %-6s   %-11s   %-9s   %-10s    %-7s   " +
+                        "%-9s   %-9s   %-8s   %-7s%n--------------------------------------------------   ----   " +
+                        "-----   ------   -----------   ---------   -----------   -------   ---------   ---------   " +
+                        "--------   -------%n", "Address", "Beds", "Baths", "SqFt", "Price", "Taxes", "$ Down",
+                "$/SqFt", "Payment", "Income", "Profit", "Acreage");
     }
 
+    /**
+     * The String method
+     * This method displays the data about each property as a string in a table
+     * @return String
+     */
+    //@Override
+    public String toString(House newHouse) {
+        Double downPayment = super.downPayment(newHouse.getPurchasePrice(), PERCENT_DOWN);
+        Double pricePerSqFoot = super.pricePerSqFt(newHouse.getPurchasePrice(), newHouse.getSqfeet());
+        Double monthlyPayment = super.monthlyPayment(newHouse.getPurchasePrice(), super.getInterestRate(),
+                super.getLoanPeriod());
+        Double rentalIncome = super.rentalIncome(newHouse.getSqfeet(), RENT_PER_SQFT);
+        Double netProfit = super.monthlyNetProfit(monthlyPayment, rentalIncome, newHouse.getTaxes());
+
+        //String of each object in arraylist that is a house
+        return String.format("%-50s   %-4d   %-5.2f   %-,6d   $%-,11.2f  $%-,9.2f  $%-,10.2f   $%-,7.2f  $%-,9.2f  " +
+                        "$%-,9.2f  $%-,8.2f   %-7.2f%n", newHouse.getAddress(), newHouse.getBedrooms(), newHouse.getBathrooms(),
+                newHouse.getSqfeet(), newHouse.getPurchasePrice(), newHouse.getTaxes(), downPayment, pricePerSqFoot,
+                monthlyPayment, rentalIncome, netProfit, newHouse.getAcreage());
+        //TODO rent per square feet?
+    }
 }
