ae3a6be4d066514c113ad63a75ed2dc892827b73
Mindy Gee <43478774+mgee4@users.noreply.github.com>
2019-12-16 20:16:45 -0600

    Updated condo to reflect current file in master

--- src/Condo.java
+++ src/Condo.java
@@ -3,13 +3,16 @@
  */
 public class Condo extends Residence {
 
+    //Yearly home owner association fee for investment property
     private double hoaFee;
+    //List of investment property amenities. Pool, tennis courts, etc.
     private String amenities;
 
     /**
      * Default constructor
      */
     Condo() {
+        //default values for hoaFee and amenities
         hoaFee = 0;
         amenities = "null";
     }
@@ -36,33 +39,25 @@ public class Condo extends Residence {
      * Return hoaFee
      * @return
      */
-    public double getHoaFee() {
-        return hoaFee;
-    }
+    public double getHoaFee() { return hoaFee; }
 
     /**
      * Set hoaFee
      * @param hoaFee
      */
-    public void setHoaFee(double hoaFee) {
-        this.hoaFee = hoaFee;
-    }
+    public void setHoaFee(double hoaFee) { this.hoaFee = hoaFee; }
 
     /**
      * Return amenities
      * @return
      */
-    public String getAmenities() {
-        return amenities;
-    }
+    public String getAmenities() { return amenities; }
 
     /**
      * Set amenities
      * @param amenities
      */
-    public void setAmenities(String amenities) {
-        this.amenities = amenities;
-    }
+    public void setAmenities(String amenities) { this.amenities = amenities; }
 
     /**
      * The monthlyNetProfit method
@@ -75,8 +70,26 @@ public class Condo extends Residence {
      * @return monthlyNetProfit
      */
     public double monthlyNetProfit(double monthlyPayment, double taxes, double rentalIncome, double hoaFee) {
+        //Equation for calculating monthly net profit
         double monthlyNetProfit = rentalIncome - (monthlyPayment + (taxes / 12) + hoaFee);
+        //Return net profit value
         return monthlyNetProfit;
+    }
+
+    /**
+     * The header Method
+     * This method returns the header for the Condo table
+     */
+    @Override
+    public String tableHeader() {
+        //Return string representation of the Multiplex table header
+        //TODO change table header from House format to Condo
+        return String.format("%nCondo Properties List%n%-50s   %-4s   %-5s   %-6s   %-11s   %-9s   %-10s    %-7s   " +
+                        "%-9s   %-9s   %-8s   %-7s   %-30s%n--------------------------------------------------   " +
+                        "----   -----   ------   -----------   ---------   -----------   -------   ---------   " +
+                        "---------   --------   -------   ------------------------------%n",
+                "Address", "Beds", "Baths", "SqFt", "Price", "Taxes", "$ Down", "$/SqFt", "Payment", "Income",
+                "Profit", "HOA Fee", "Amenities");
     }
 
     /**
@@ -84,9 +97,20 @@ public class Condo extends Residence {
      * This method display the data about each property as a string in a table
      * @return String
      */
-    @Override
-    public String toString() {
+    //@Override
+    public String toString(Condo newCondo) {
+        Double downPayment = super.downPayment(newCondo.getPurchasePrice(), PERCENT_DOWN);
+        Double pricePerSqFoot = super.pricePerSqFt(newCondo.getPurchasePrice(), newCondo.getSqfeet());
+        Double monthlyPayment = super.monthlyPayment(newCondo.getPurchasePrice(), super.getInterestRate(),
+                super.getLoanPeriod());
+        Double rentalIncome = super.rentalIncome(newCondo.getSqfeet(), RENT_PER_SQFT);
+        Double netProfit = super.monthlyNetProfit(monthlyPayment, rentalIncome, newCondo.getTaxes());
 
-        return String.format(" ");
+        //Returns the string for each object of a Multiplex to the table
+        return String.format("%-50s   %-4d   %-5.2f   %-,6d   $%-,11.2f  $%-,9.2f  $%-,10.2f   $%-,7.2f  $%-,9.2f  " +
+                        "$%-,9.2f  $%-,7.2f   $%-7.2f   %-30s%n", newCondo.getAddress(), newCondo.getBedrooms(),
+                newCondo.getBathrooms(), newCondo.getSqfeet(), newCondo.getPurchasePrice(), newCondo.getTaxes(),
+                downPayment, pricePerSqFoot, monthlyPayment, rentalIncome, netProfit, newCondo.getHoaFee(),
+                newCondo.getAmenities());
     }
 }
