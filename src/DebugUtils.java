39eb11c48014ac1b8e557aca6d065db0cba9861d
breanna-hansen <breannahansen@gmail.com>
2019-12-17 15:14:38 -0700

    remove Debugutils

--- src/DebugUtils.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class DebugUtils {
-    public static void Write(ArrayList<Residence> newData) {
-        for(Residence r: newData){
-            System.out.println(r.getStringValue());
-        }
-    }
-}
